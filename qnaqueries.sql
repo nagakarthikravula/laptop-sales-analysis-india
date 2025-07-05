0154af668f471376d305dddcc89b305fbc18e12a
Ravula Naga Karthik <nagakarthikravula@gmail.com>
2025-07-05 18:28:00 +0530

    Update qnaqueries.sql

--- qnaqueries.sql
+++ qnaqueries.sql
@@ -1,1 +1,3 @@
+--Question 1: Age Distribution of Customers
+select age as Age,Count(*) as Purchase_Count from `laptop-sales-analysis-india.laptop_sales_dataset.laptop_sales_data` group by age order by age;
 
